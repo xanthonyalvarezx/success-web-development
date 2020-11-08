32e453c392fedca7ea83e12fc20288f77d4abcf5
AntMan <xanthonyalvarezx@gmail.com>
2020-11-08 12:31:04 -0500

    ran npm run build

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
-import './Stylesheets/App.css'
-import { Button, Icon, } from "semantic-ui-react";
-import Main from './pages/Main'
-import PreviousProjects from './pages/PreviousProjects'
+import React, { Component } from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import "./Stylesheets/App.css";
+import { Button, Icon } from "semantic-ui-react";
+import Main from "./pages/Main";
+import PreviousProjects from "./pages/PreviousProjects";
 
-import Nav from './components/Nav'
-import MeetUS from './pages/meetUs';
-import Contact from './pages/ContactPage';
-
+import Nav from "./components/Nav";
+import MeetUS from "./pages/meetUs";
+import Contact from "./pages/ContactPage";
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
@@ -19,34 +17,35 @@ class App extends Component {
           <Nav />
 
           <Switch>
-            <Route exact path='/' component={Main} />
+            <Route exact path="/" component={Main} />
 
-            <Route exact path='/Contact' component={Contact} />
+            <Route exact path="/Contact" component={Contact} />
 
-            <Route exact path='/Previous-Projects' component={PreviousProjects} />
+            <Route exact path="/Previous-Projects" component={PreviousProjects} />
 
-            <Route exact path='/Meet-us' component={MeetUS} />
-
-
-
-
+            <Route exact path="/Meet-us" component={MeetUS} />
           </Switch>
-
         </Router>
         <footer>
-          <div id='socialButtons'>
-            <Button color='facebook'>
-              <Icon name='facebook' />
-              <a href='https://www.facebook.com/Success-Web-Development-106136581297061/?notif_id=1603832810863896&notif_t=page_fan&ref=notif' target='blank'>FaceBook</a>
+          <div id="socialButtons">
+            <Button color="facebook">
+              <Icon name="facebook" />
+              <a
+                href="https://www.facebook.com/Success-Web-Development-106136581297061/?notif_id=1603832810863896&notif_t=page_fan&ref=notif"
+                target="blank"
+              >
+                FaceBook
+              </a>
             </Button>
-            <Button color='instagram'>
-              <Icon name='instagram' /> Instagram
-    </Button>
+            <Button color="instagram">
+              <Icon name="instagram" />
+              <a href="https://www.instagram.com/successwebdevelopment/" target="blank">
+                Instagram
+              </a>
+            </Button>
           </div>
-
         </footer>
       </div>
-
     );
   }
 }
