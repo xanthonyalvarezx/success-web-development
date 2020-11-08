231d2b67c0efd7b0d0d245c703fcd92dd167f46d
AntMan <xanthonyalvarezx@gmail.com>
2020-11-08 13:46:55 -0500

    testing

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,55 +1,25 @@
-import React, { Component } from 'react'
-import Hero from '../images/Hero.png'
-import Logo from '../images/Logo.png'
+import React, { Component } from "react";
+import Hero from "../images/Hero.png";
+import Logo from "../images/Logo.png";
 
-import '../Stylesheets/main.css'
+import "../Stylesheets/main.css";
 
 class Main extends Component {
+  render() {
+    return (
+      <>
+        <header>
+          <img id="hero" src={Hero} alt='>>> print("HELLO WORLD")' />
+        </header>
 
-
-
-
-
-
-
-
-
-
-    render() {
-        return (
-
- 
-            <>
-                <header>
-                    <img id='hero' src={Hero} alt='>>> print("HELLO WORLD")' />
-                </header>
-
-  
- 
-
-                <main>
-                    <div className='logo-image'>
-                        <img id='logo' src={Logo} alt='Success Web Development logo' />
-                    </div>
-                </main>
-            </>
-
-
-
-
-
-
-
-
-
-
-
-
-
-        )
-
-
-    }
+        <main>
+          <div className="logo-image">
+            <img id="logo" src={Logo} alt="Success Web Development logo" />
+          </div>
+        </main>
+      </>
+    );
+  }
 }
 
-export default Main
+export default Main;
